31148f882c514f5d1938fec2b30d32b7b256c3a4
tireks <33616425+tireks@users.noreply.github.com>
2024-05-31 23:05:29 +0700

    это все что нужно было по базовой задаче?

--- app/src/main/java/com/tirexmurina/leonov/MainActivity.kt
+++ app/src/main/java/com/tirexmurina/leonov/MainActivity.kt
@@ -1,20 +1,59 @@
 package com.tirexmurina.leonov
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.tirexmurina.leonov.databinding.ActivityMainBinding
+import java.util.Timer
+import java.util.TimerTask
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val mainHandler = Handler(Looper.getMainLooper())
+    private var timer: Timer? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.timerButton.setOnClickListener {
+            startTimer()
         }
     }
+
+    private fun startTimer() {
+        timer?.cancel()
+        timer = Timer()
+
+        var secondsLeft = 10
+
+        val timerTask = object : TimerTask() {
+            override fun run() {
+                if (secondsLeft > 0) {
+                    mainHandler.post {
+                        binding.timerText.text = "Timer: $secondsLeft"
+                    }
+                    secondsLeft--
+                } else {
+                    timer?.cancel()
+                    mainHandler.post {
+                        binding.timerText.text = getString(R.string.timerText_label)
+                    }
+                }
+            }
+        }
+
+        timer?.schedule(timerTask, 0, 1000)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        timer?.cancel()
+    }
+
+
 }
